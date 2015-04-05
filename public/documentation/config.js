bff4ef57c3ff6684c7c4b566a7f4028fc51afdaa
Christoph Kappestein <k42b3.x@gmail.com>
2015-04-05 12:34:13 +0200

    add evid api documentation viewer

---
+++ public/documentation/config.js
@@ -0,0 +1,14 @@
+
+var evid_config = {
+
+	/**
+	 * Path to the PSX documentation endpoint
+	 */
+	url: '/projects/psx-sample/public/index.php/tool/doc',
+
+	/**
+	 * Paths which should be excluded from the documentation
+	 */
+	exclude: []
+
+};
